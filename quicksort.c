905a9dc61bfdee78bbe52e61470b8f08b8c6ac97
smng0 <1155158261@link.cuhk.edu.hk>
2025-02-28 03:12:23 +0800

    Quicksort sample code

---
+++ quicksort.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+typedef int ElementType;
+
+// A helper function that swaps two elements in an array
+void swap(ElementType *arr, int a, int b) {
+    ElementType tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+}
+
+// A function that performs partition on an array
+int partition(ElementType *arr, int left, int right) {
+    int pivot = arr[left]; // Key value of pivot (First element as pivot)
+    int l = left; // Left pointer
+    int r = right; // Right pointer
+
+    while (l < r) {
+        while (arr[l] <= pivot && l < r) {
+            l++; // Move to the right
+        }
+        while (arr[r] > pivot) {
+            r--; // Move to the left
+        }
+        if (l < r) {
+            swap(arr, l, r); // Swap
+        }
+    }
+
+    arr[left] = arr[r];
+    arr[r] = pivot;
+    return r; // New position of pivot
+}
+
+// A function that performs quick sort on an array
+void q_sort(ElementType *arr, int left, int right) {
+    int pivot; // Position of the pivot after partition
+
+    if (left < right) { // Not the base case
+        // Divide: Partition
+        pivot = partition(arr, left, right);
+
+        // Conquer: Solve sub-problems
+        q_sort(arr, left, pivot-1);
+        q_sort(arr, pivot+1, right);
+    }
+}
+
+// A wrapper function that performs quicksort on an array
+void quicksort(int n, ElementType *arr) {
+    q_sort(arr, 0, n-1);
+}
+
+int main() {
+    int i; // Loop variable
+
+    ElementType arr[] = {19, 25, 14, 1, 26, 22, 5, 27, 3};    
+    quicksort(9, arr);
+    for (i = 0; i < 9; i++) {
+        printf("%d ", arr[i]);
+    }
+    putchar('\n');
+    return 0;
+}
